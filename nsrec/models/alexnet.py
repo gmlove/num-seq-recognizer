2c0a094866999c8fed17a4fcf8a828e383d020fa
gmlove <gracekinglau@gmail.com>
2016-12-24 15:34:15 +0800

    two steps alex net

---
+++ nsrec/models/alexnet.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+from tensorflow.contrib import slim
+
+trunc_normal = lambda stddev: tf.truncated_normal_initializer(0.0, stddev)
+
+def cnn_layers(inputs, scope, end_points_collection):
+  # Collect outputs for conv2d and max_pool2d.
+  with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
+                      outputs_collections=[end_points_collection]):
+    net = slim.conv2d(inputs, 64, [11, 11], 4, padding='VALID',
+                      scope='conv1')
+    net = slim.max_pool2d(net, [3, 3], 2, scope='pool1')
+    net = slim.conv2d(net, 192, [5, 5], scope='conv2')
+    net = slim.max_pool2d(net, [3, 3], 2, scope='pool2')
+    net = slim.conv2d(net, 384, [3, 3], scope='conv3')
+    net = slim.conv2d(net, 384, [3, 3], scope='conv4')
+    net = slim.conv2d(net, 256, [3, 3], scope='conv5')
+    net = slim.max_pool2d(net, [3, 3], 2, scope='pool5')
+
+  end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+  return net, end_points
+
+def variable_scope(values=None, name_or_scope=None, **kwargs):
+  values = values if isinstance(values, (tuple, list)) else [values]
+  kwargs.update({'values': values, 'default_name': 'alexnet_v2'})
+  return tf.variable_scope(name_or_scope, **kwargs)
+
+def end_points_collection_name(variable_scope):
+  end_points_collection = variable_scope.original_name_scope + '_end_points'
+  return end_points_collection
+
+
+def fc_layers(net,
+              scope,
+              end_points_collection,
+              num_classes=1000,
+              is_training=True,
+              dropout_keep_prob=0.5,
+              spatial_squeeze=True,
+              name_prefix=None):
+  full_scope_name = lambda scope_name: scope_name if name_prefix is None else '%s_%s' % (name_prefix, scope_name)
+  # Use conv2d instead of fully_connected layers.
+  with slim.arg_scope([slim.conv2d],
+                      weights_initializer=trunc_normal(0.005),
+                      biases_initializer=tf.constant_initializer(0.1),
+                      outputs_collections=[end_points_collection]):
+    net = slim.conv2d(net, 4096, [5, 5], padding='VALID',
+                      scope=full_scope_name('fc6'))
+    net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                       scope=full_scope_name('dropout6'))
+    net = slim.conv2d(net, 4096, [1, 1], scope=full_scope_name('fc7'))
+    net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                       scope=full_scope_name('dropout7'))
+    net = slim.conv2d(net, num_classes, [1, 1],
+                      activation_fn=None,
+                      normalizer_fn=None,
+                      biases_initializer=tf.zeros_initializer,
+                      scope=full_scope_name('fc8'))
+
+  # Convert end_points_collection into a end_point dict.
+  end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+  if spatial_squeeze:
+    net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
+    end_points[scope.name + '/fc8'] = net
+  return net, end_points
---
+++ nsrec/models/alexnet_test.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+from nsrec.models import alexnet
+
+class AlexnetTest(tf.test.TestCase):
+
+  def test_variable_scope(self):
+    with self.test_session():
+      inputs = tf.constant(1)
+      with alexnet.variable_scope(inputs) as variable_scope:
+        self.assertEqual(variable_scope.name, 'alexnet_v2')
+        v = tf.get_variable('test', [1])
+        self.assertIs(v.graph, inputs.graph)
+
+  def test_variable_scope_with_array(self):
+    with self.test_session():
+      inputs = tf.constant(1)
+      with alexnet.variable_scope([inputs]) as variable_scope:
+        self.assertEqual(variable_scope.name, 'alexnet_v2')
+        v = tf.get_variable('test', [1])
+        self.assertIs(v.graph, inputs.graph)
+
+  def test_cnn_layers_end_points(self):
+    batch_size, height, width, channels = 5, 224, 224, 3
+    with self.test_session():
+      inputs = tf.random_uniform((batch_size, height, width, channels))
+      with alexnet.variable_scope([inputs]) as variable_scope:
+        end_points_collection = alexnet.end_points_collection_name(variable_scope)
+        _, end_points = alexnet.cnn_layers(inputs, variable_scope, end_points_collection)
+
+      expected_names = [
+        'alexnet_v2/conv1',
+        'alexnet_v2/pool1',
+        'alexnet_v2/conv2',
+        'alexnet_v2/pool2',
+        'alexnet_v2/conv3',
+        'alexnet_v2/conv4',
+        'alexnet_v2/conv5',
+        'alexnet_v2/pool5',
+      ]
+      self.assertSetEqual(set(end_points.keys()), set(expected_names))
+
+  def test_fc_layers_end_points(self):
+    layers_name_prefix = "length"
+    batch_size, height, width, channels = 5, 224, 224, 3
+    with self.test_session():
+      inputs = tf.random_uniform((batch_size, height, width, channels))
+      with alexnet.variable_scope([inputs]) as variable_scope:
+        end_points_collection = alexnet.end_points_collection_name(variable_scope)
+        net, _ = alexnet.cnn_layers(inputs, variable_scope, end_points_collection)
+        _, end_points = alexnet.fc_layers(net, variable_scope, end_points_collection, num_classes=10, name_prefix=layers_name_prefix)
+
+      expected_names = [
+        'alexnet_v2/%s_fc6' % layers_name_prefix,
+        'alexnet_v2/%s_fc7' % layers_name_prefix,
+        'alexnet_v2/%s_fc8' % layers_name_prefix
+      ]
+      self.assertSetEqual(set(filter(lambda x: x.find('_fc') != -1, end_points.keys())),
+                          set(expected_names))
+
+
+if __name__ == '__main__':
+  tf.test.main()
