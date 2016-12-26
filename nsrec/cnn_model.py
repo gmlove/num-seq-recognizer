a19ca937af5b34aebd1b2234a953862193089893
gmlove <gracekinglau@gmail.com>
2016-12-26 14:11:40 +0800

    cnn training model

---
+++ nsrec/cnn_model.py
@@ -0,0 +1,82 @@
+import os
+import tensorflow as tf
+
+from nsrec import inputs
+from nsrec.nets import alexnet
+
+
+class CNNModelConfig(object):
+
+  def __init__(self, **kwargs):
+    self.data_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data/train')
+    self.metadata_file_path = os.path.join(self.data_dir_path, 'digitStruct.mat')
+    self.max_number_length = 5
+    self.batch_size = 64
+    self.size = [alexnet.image_height, alexnet.image_width]
+
+    for attr in ['data_dir_path', 'metadata_file_path', 'max_number_length', 'batch_size', 'size']:
+      setattr(self, attr, kwargs.get(attr, getattr(self, attr)))
+
+
+class CNNModelBase:
+
+  def __init__(self, config):
+    self.config = config
+    self.variables = None
+    self.data_batches = None
+
+  def _build_base_net(self, sess):
+    sess.as_default()
+    with alexnet.variable_scope([self.data_batches]) as variable_scope:
+      end_points_collection = alexnet.end_points_collection_name(variable_scope)
+      net, _ = alexnet.cnn_layers(self.data_batches, variable_scope, end_points_collection)
+      length_output, _ = alexnet.fc_layers(net, variable_scope, end_points_collection,
+                                           num_classes=self.config.max_number_length,
+                                           name_prefix='length')
+      numbers_output = []
+      for i in range(self.config.max_number_length):
+        number_output, _ = alexnet.fc_layers(net, variable_scope, end_points_collection,
+                                             num_classes=10, name_prefix='number%s' % (i + 1))
+        numbers_output.append(number_output)
+
+      return length_output, numbers_output
+
+
+class CNNTrainModel(CNNModelBase):
+
+  def __init__(self, config):
+    self.config = config
+    self.data_batches, self.length_label_batches, self.numbers_label_batches = \
+      inputs.batches(config.metadata_file_path, config.data_dir_path,
+                    config.max_number_length, config.batch_size, config.size)
+    self.total_loss = None
+    self.global_step = None
+
+  def build(self, sess):
+    length_output, numbers_output = self._build_base_net(sess)
+    length_loss = tf.nn.softmax_cross_entropy_with_logits(length_output, self.length_label_batches)
+    tf.contrib.losses.add_loss(tf.log(tf.reduce_mean(length_loss), 'length_loss'))
+    for i in range(self.config.max_number_length):
+      number_loss = tf.nn.softmax_cross_entropy_with_logits(numbers_output[i], self.numbers_label_batches[:,i,:])
+      tf.contrib.losses.add_loss(tf.log(tf.reduce_mean(number_loss), 'number%s_loss' % (i + 1)))
+    self.total_loss = tf.contrib.losses.get_total_loss()
+
+    self.setup_global_step()
+
+  def setup_global_step(self):
+    """Sets up the global step Tensor."""
+    global_step = tf.Variable(
+      initial_value=0,
+      name="global_step",
+      trainable=False,
+      collections=[tf.GraphKeys.GLOBAL_STEP, tf.GraphKeys.GLOBAL_VARIABLES])
+
+    self.global_step = global_step
+
+
+class CNNEvalModel(CNNModelBase):
+  pass
+
+
+class CNNPredictModel(CNNModelBase):
+  pass
---
+++ nsrec/cnn_model_test.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+from nsrec.cnn_model import *
+from nsrec.data_reader_test import DataReaderTest
+
+
+class CNNModelTest(tf.test.TestCase):
+
+  def test_train_model(self):
+    metadata_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_data')
+    metadata_file_path = DataReaderTest.createTestData(25, metadata_dir_path)
+    config = CNNModelConfig(metadata_file_path=metadata_file_path, batch_size=2)
+
+    with self.test_session() as sess:
+      model = CNNTrainModel(config)
+      model.build(sess)
+
+      train_op = tf.contrib.layers.optimize_loss(
+        loss=model.total_loss, global_step=model.global_step,
+        learning_rate=0.1, optimizer=tf.train.MomentumOptimizer(0.5, momentum=0.5))
+      tf.contrib.slim.learning.train(
+        train_op, None, number_of_steps=2)
+
+if __name__ == '__main__':
+  tf.test.main()
+
