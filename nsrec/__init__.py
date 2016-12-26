916d0f19fa7fee30b580efeaab9be16e508e3c9a
gmlove <gracekinglau@gmail.com>
2016-12-26 20:17:00 +0800

    move logging config into __init__

--- nsrec/__init__.py
+++ nsrec/__init__.py
@@ -1,1 +1,7 @@
-from nsrec import log_config
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.flags.DEFINE_string('log_level', 'info', 'Log level.')
+
+tf.logging.set_verbosity(getattr(tf.logging, FLAGS.log_level.upper()))
