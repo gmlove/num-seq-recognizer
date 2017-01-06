73bcb573c047a0c04b2213ba2e89e3356964a1ac
gmlove <gracekinglau@gmail.com>
2017-01-06 17:50:18 +0800

    add improved lenet model

--- nsrec/nets/lenet_v1.py
+++ nsrec/nets/lenet_v1.py
@@ -6,8 +6,11 @@ default_scope_name = 'lenet_v1'
 image_height, image_width = 64, 64
 variable_scope = variable_scope_fn(default_scope_name)
 
+'''
+accuracy: 0.68, data: 4.2M
+'''
 
-def cnn_layers(inputs, scope, end_points_collection):
+def cnn_layers(inputs, scope, end_points_collection, dropout_keep_prob=0.8, is_training=True):
   with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
                       outputs_collections=[end_points_collection]):
     net = slim.conv2d(inputs, 32, [5, 5], scope='conv1')
@@ -22,7 +25,7 @@ def cnn_layers(inputs, scope, end_points_collection):
     net = slim.conv2d(net, 64, [5, 5], scope='conv7')
     net = slim.flatten(net)
 
-    net = slim.fully_connected(net, 256, scope='fc3')
+    net = slim.fully_connected(net, 128, scope='fc3')
 
   return net, end_points_collection
 
--- nsrec/nets/lenet_v2.py
+++ nsrec/nets/lenet_v2.py
@@ -2,12 +2,16 @@ from tensorflow.contrib import slim
 
 from nsrec.nets import variable_scope_fn, end_points_collection_name
 
-default_scope_name = 'lenet_v1'
+default_scope_name = 'lenet_v2'
 image_height, image_width = 64, 64
 variable_scope = variable_scope_fn(default_scope_name)
 
+'''
+best accuracy: 0.71, data: 6.3M
+'''
 
-def cnn_layers(inputs, scope, end_points_collection):
+
+def cnn_layers(inputs, scope, end_points_collection, dropout_keep_prob=0.8, is_training=True):
   with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
                       outputs_collections=[end_points_collection]):
     net = slim.conv2d(inputs, 32, [5, 5], scope='conv1')
@@ -22,7 +26,7 @@ def cnn_layers(inputs, scope, end_points_collection):
     net = slim.conv2d(net, 64, [5, 5], scope='conv7')
     net = slim.flatten(net)
 
-    net = slim.fully_connected(net, 128, scope='fc3')
+    net = slim.fully_connected(net, 256, scope='fc3')
 
   return net, end_points_collection
 
@@ -40,8 +44,12 @@ def fc_layers(net,
 
   with slim.arg_scope([slim.fully_connected, slim.dropout],
                       outputs_collections=[end_points_collection]):
-    net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-                       scope=full_scope_name('dropout3'))
+    '''
+    with dropout: accuracy: 0.7, data: 6.3M
+    without dropout: accuracy: 0.71, data: 6.3M
+    '''
+    # net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+    #                    scope=full_scope_name('dropout3'))
     net = slim.fully_connected(net, num_classes, activation_fn=None,
                                scope=full_scope_name('fc4'))
 
